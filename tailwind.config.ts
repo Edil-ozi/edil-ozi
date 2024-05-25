4057de296463b365b3ad184ecef7c125b261a0f5
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-05-25 14:55:05 +0500

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+
   },
   plugins: [],
 };
