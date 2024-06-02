145d2149282128383c74a973ebe8edb94d2e2760
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-06-02 22:40:04 +0500

    refactor: pricing-card

--- registry/components/edil-ozi/pricing-card.tsx
+++ registry/components/edil-ozi/pricing-card.tsx
@@ -1,48 +1,56 @@
 import { FC } from "react";
 
-interface Props {
+type Card = {
   heading: string;
   description: string;
   price: number;
-  discount?: number;
-  list: string[];
-  listHeading?:string;
   buttonText: string;
+  list: string[];
+  discount?: number;
+  listHeading?: string;
   className?: string;
   onClick?: () => void;
 }
 
-function withDiscount(price:number, discount:number) {
-  const res = price - ((price * discount) / 100);
-  return res
+interface Props extends Card {
+
 }
 
+
 const PricingCard: FC<Props> = ({ heading, description, price, discount, list, listHeading, buttonText, className, onClick }) => {
+
+  const withDiscount = (function () {
+    return price - ((price * (discount ?? 100)) / 100);
+  })()
   return (
-    <div className={`bg-gradient-to-br h-fit w-fit from-indigo-900 to-gray-900 relative border xl:min-w-[310px] min-w-56 border-gray-800 rounded-lg shadow-lg p-6 lg:p-8 transform hover:scale-105 transition duration-300 overflow-hidden ${className}`}>
+    <div className={`bg-gradient-to-br w-full max-w-[300px] from-indigo-900 to-gray-900 relative border xl:min-w-[310px] min-w-56 border-gray-800 rounded-lg shadow-lg p-6 lg:p-8 transform hover:scale-[none] md:hover:scale-105 transition duration-300 overflow-hidden ${className}`}>
+      <div className="flex flex-col justify-between h-full">
+
       <div className="mb-4 lg:mb-6 xl:mb-8">
-        <h3 className="text-2xl lg:text-3xl xl:text-4xl font-semibold text-white">{heading}</h3>
-        <p className="mt-2 lg:mt-4 text-gray-300 lg:text-xl xl:text-2xl">{description}</p>
+          <h3 className="text-2xl lg:text-3xl xl:text-4xl font-semibold mb-2 lg:mb-4">{heading}</h3>
+          <p className=" text-gray-300 lg:text-base xl:text-xl">{description}</p>
       </div>
-      <div className="mb-6 flex space-x-2 xl:mb-8">
-        <span className="text-3xl lg:text-4xl xl:text-5xl font-extrabold text-white">${(!discount ? price : withDiscount(price, discount)).toFixed(2)}</span>
-        {discount && <span className="text-gray-300 line-through md:text-lg lg:text-xl xl:text-2xl">${(price * discount / 100).toFixed(0)}</span>}
+        <div>
+
+          <div className="mb-3 flex space-x-2 xl:mb-4">
+            <span className="text-3xl lg:text-4xl xl:text-5xl font-extrabold ">${(discount ? withDiscount : price)}</span>
+            {discount && <span className="text-gray-400 line-through md:text-lg lg:text-xl xl:text-2xl">{price}$</span>}
       </div>
       {discount && <div className="top-0 right-[-50%] lg:text-lg xl:text-xl -translate-x-6 translate-y-4 origin-center-right w-full text-center rotate-45 bg-gradient-to-r from-blue-500 to-purple-500 absolute ">
         {discount}%
       </div>}
       <button
       onClick={onClick}
-        className="w-full mb-4 lg:mb-6 xl:mb-8 py-2 px-6 text-center sm:text-lg lg:text-xl xl:text-2xl rounded-md text-white font-medium bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600"
+            className="w-full py-2 px-6 text-center sm:text-lg lg:text-xl xl:text-2xl rounded-md text-white font-medium bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600"
       >
         {buttonText}
       </button>
-      <ul className={`space-y-1 text-gray-200 lg:text-lg xl:text-xl ${list}`}>
-        <li>{listHeading}</li>
+          <ul className={`space-y-1 text-gray-200 lg:text-lg xl:text-xl mt-4 lg:mt-6 xl:mt-8 `}>
+            <h5>{listHeading}</h5>
         {list.map((text, index) => (
           <li
             key={index}
-            className={`flex items-center ${list}`}
+            className={`flex items-center `}
           >
             <svg
               className="h-5 w-5 text-green-400 mr-2"
@@ -62,6 +70,8 @@ const PricingCard: FC<Props> = ({ heading, description, price, discount, list, l
           </li>
         ))}
       </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- registry/components/example/pricing-card-demo.tsx
+++ registry/components/example/pricing-card-demo.tsx
@@ -1,34 +1,53 @@
-import PricingCard from "../edil-ozi/pricing-card";
+import PricingCard from "@/registry/components/edil-ozi/pricing-card";
+
+type Card = {
+  heading: string;
+  description: string;
+  price: number;
+  buttonText: string;
+  list: string[];
+  discount?: number;
+  listHeading?: string;
+  className?: string;
+  onClick?: () => void;
+}
 
 export default function PricingCardDemo() {
+
+  const cards: Card[] = [
+    {
+      heading: "Room 1",
+      description: "One of the best rooms longer text sample ",
+      price: 299,
+      discount: 40,
+      list: ["Clean", "Soft", "With beautiful view", "Bedroom"],
+      buttonText: "Book now!",
+      className: '',
+    },
+    {
+      heading: "Room 2",
+      description: "One of the best rooms",
+      price: 399.45,
+      list: ["Clean", "Soft", "With beautiful view", "Bedroom"],
+      buttonText: "Book now!",
+      className: '',
+    },
+    {
+      heading: "Room 3",
+      description: "One of the best rooms",
+      price: 499,
+      discount: 15,
+      list: ["Clean", "Soft", "With beautiful view", "Bedroom"],
+      buttonText: "Book now!",
+      className: '',
+    },
+  ]
+
   return (
-    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 xl:grid-cols-3">
-      <PricingCard
-        heading="Room 1"
-        description="One of the best rooms"
-        price={299}
-        discount={40}
-        list={["Clean", "Soft", "With beautiful view", "Bedroom"]}
-        buttonText="Book now!"
-      />
-      <PricingCard
-        heading="Room 2"
-        description="One of the best rooms"
-        price={399.45}
-        listHeading="Why should you choose us:"
-        list={["Clean", "Soft", "With beautiful view", "Bedroom"]}
-        buttonText="Book now!"
-      />
-      <PricingCard
-      className="hidden xl:block"
-        heading="Room 3"
-        description="One of the best rooms"
-        price={499}
-        discount={15}
-        listHeading="Why should you choose us:"
-        list={["Clean", "Soft", "With beautiful view", "Bedroom"]}
-        buttonText="Book now!"
-      />
+    <div className="flex flex-wrap gap-6 justify-center">
+      {cards.map(card => (
+        <PricingCard key={card.heading} {...card} />
+      ))}
     </div>
   );
 }
