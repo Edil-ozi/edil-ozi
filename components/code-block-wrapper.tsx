6becbb853cbdce0fd327f753732c64ef2b212008
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-06-06 18:13:33 +0500

    fix gradient colors

--- components/code-block-wrapper.tsx
+++ components/code-block-wrapper.tsx
@@ -36,7 +36,7 @@ export function CodeBlockWrapper({ expandButtonTitle = "View Code", className, c
         </CollapsibleContent>
         <div
           className={cn(
-            "absolute flex items-center justify-center bg-gradient-to-b from-background/10 to-background to-90% p-2",
+            "absolute flex items-center justify-center bg-gradient-to-b from-black/10 to-black to-90% p-2",
             isOpened ? "inset-x-0 bottom-0 h-12 from-gray-900/30" : "inset-0",
           )}
         >
