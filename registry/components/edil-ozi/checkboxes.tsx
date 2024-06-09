b47c648e50d93fb6c182897557f7b5148cab6f64
karadza_b <bayezitkarajaew@gmail.com>
2024-06-09 16:55:44 +0500

    fix: some color

--- registry/components/edil-ozi/checkboxes.tsx
+++ registry/components/edil-ozi/checkboxes.tsx
@@ -13,7 +13,11 @@ const Checkboxes = () => {
             className="peer hidden"
             type="checkbox"
           />
-          <div className="duration-[250ms] after:duration-[250ms,border-width_0.1s] after:delay-[0.1s] relative left-0 top-0 h-[1.6rem] w-[1.6rem] rounded-[0.3em] bg-white transition-all after:absolute after:left-0 after:top-0 after:h-[1.6rem] after:w-[1.6rem] after:rotate-0 after:rounded-[0.25em] after:border-[2px] after:border-[rgba(0,0,0,0.863)] after:transition-all after:content-[''] peer-checked:bg-black peer-checked:shadow-[-10px_-10px_50px_0px_rgb(17,0,248),0_-10px_50px_0px_rgb(243,11,243),10px_-10px_50px_0px_rgb(253,228,0),10px_0_50px_0px_rgb(107,255,21),10px_10px_50px_0px_rgb(76,0,255),0_10px_50px_0px_rgb(255,196,0),-10px_10px_50px_0px_rgb(90,105,240)] peer-checked:after:left-2 peer-checked:after:top-[1px] peer-checked:after:h-[0.6em] peer-checked:after:w-[0.35em] peer-checked:after:rotate-45 peer-checked:after:rounded-[0em] peer-checked:after:border-b-[0.1em] peer-checked:after:border-r-[0.1em] peer-checked:after:border-[rgba(238,238,238,0)_white_white_#fff0] dark:bg-black dark:after:border-[rgba(255,255,255,0.863)] dark:peer-checked:bg-white dark:peer-checked:after:border-[rgba(238,238,238,0)_black_black_#fff0]"></div>
+          <div className="duration-[250ms] after:duration-[250ms,border-width_0.1s] after:delay-[0.1s] relative left-0 top-0 h-[1.6rem] w-[1.6rem] rounded-[0.3em] bg-white transition-all after:absolute after:left-0 after:top-0 after:h-[1.6rem] after:w-[1.6rem] after:rotate-0 after:rounded-[0.25em] after:border-[2px] after:border-[rgba(0,0,0,0.863)] after:transition-all after:content-[''] peer-checked:bg-black
+
+          peer-checked:shadow-[-13px_-13px_40px_0px_rgb(17,0,248),13px_-0_40px_0px_rgb(243,11,243),13px_-13px_40px_0px_rgb(253,228,0),13px_0_40px_0px_rgb(107,255,21),13px_13px_40px_0px_rgb(76,0,255),13px_13px_40px_0px_rgb(255,196,0),-13px_13px_40px_0px_rgb(90,105,240)]
+
+          peer-checked:after:left-2 peer-checked:after:top-[1px] peer-checked:after:h-[0.6em] peer-checked:after:w-[0.35em] peer-checked:after:rotate-45 peer-checked:after:rounded-[0em] peer-checked:after:border-b-[0.1em] peer-checked:after:border-r-[0.1em] peer-checked:after:border-[rgba(238,238,238,0)_white_white_#fff0] dark:bg-black dark:after:border-[rgba(255,255,255,0.863)] dark:peer-checked:bg-white dark:peer-checked:after:border-[rgba(238,238,238,0)_black_black_#fff0]"></div>
         </label>
       ),
       message: "Successfulty copied",
