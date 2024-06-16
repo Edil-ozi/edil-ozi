23a096210eebfc0387b6cd60f644460bf646f2af
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-06-16 20:05:59 +0500

    add canvas cursor (undone)

---
+++ registry/components/example/canvas-cursor-demo.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import CanvasCursor from '@/registry/components/edil-ozi/canvas-cursor';
+
+
+const CanvasCursorDemo = () => {
+  return <CanvasCursor />
+};
+export default CanvasCursorDemo
