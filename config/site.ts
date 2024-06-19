fdc80d6bc6c5f9c5a64353eacf332c8a9da81616
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-06-19 21:35:54 +0500

    add favicon; og image

--- config/site.ts
+++ config/site.ts
@@ -1,10 +1,10 @@
 export const siteConfig = {
   name: "Edil Ozi",
-  url: "https://edilozi.com",
+  url: "https://edilozi.pro/og.png",
   description: "Beautiful, Open Source components that you can copy and paste into your projects.",
   links: {
     edilozi: "https://edilozi.com",
-    github: "https://github.com/Shatlyk1011/edil-ozi",
+    github: "https://github.com/Edil-ozi/edil-ozi",
   },
 };
 
