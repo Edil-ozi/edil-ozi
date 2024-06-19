3af2a2757006cbaf6e089bc7c974ffa6d600ee84
Abdullayev Shatlyk <gj_wp@mail.ru>
2024-06-19 20:38:24 +0500

    add new logo

--- components/icons.tsx
+++ components/icons.tsx
@@ -3,49 +3,17 @@ type IconProps = React.HTMLAttributes<SVGElement>;
 
 export const Icons = {
   logo: (props: IconProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
-      {...props}
-    >
-      <rect
-        width="256"
-        height="256"
-        fill="none"
-      />
-      <line
-        x1="208"
-        y1="128"
-        x2="128"
-        y2="208"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="16"
-      />
-      <line
-        x1="192"
-        y1="40"
-        x2="40"
-        y2="192"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="16"
-      />
-    </svg>
-  ),
-  twitter: (props: IconProps) => (
-    <svg
-      {...props}
-      height="23"
-      viewBox="0 0 1200 1227"
-      width="23"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
+    <svg width="100" height="100" viewBox="0 0 100 100" fill="red" {...props} xmlns="http://www.w3.org/2000/svg">
+      <line x1="86.5" y1="16" x2="86.5" y2="84" stroke="currentColor" stroke-width="5" />
+      <line x1="69" y1="16" x2="69" y2="84" stroke="currentColor" stroke-width="4" />
+      <line x1="51.5" y1="16" x2="51.5" y2="84" stroke="currentColor" stroke-width="3" />
+      <line x1="34" y1="16" x2="34" y2="84" stroke="currentColor" stroke-width="2" />
+      <line x1="16.5" y1="16" x2="16.5" y2="84" stroke="currentColor" />
+      <circle cx="78" cy="22" r="6" transform="rotate(-180 78 22)" fill="currentColor" />
+      <circle cx="62" cy="33" r="5" transform="rotate(-180 62 33)" fill="currentColor" />
+      <circle cx="46" cy="42" r="4" transform="rotate(-180 46 42)" fill="currentColor" />
+      <circle cx="14" cy="54" r="2" transform="rotate(-180 14 54)" fill="currentColor" />
+      <circle cx="30" cy="49" r="3" transform="rotate(-180 30 49)" fill="currentColor" />
     </svg>
   ),
   gitHub: (props: IconProps) => (
--- config/site.ts
+++ config/site.ts
@@ -4,7 +4,6 @@ export const siteConfig = {
   description: "Beautiful, Open Source components that you can copy and paste into your projects.",
   links: {
     edilozi: "https://edilozi.com",
-    // twitter: "https://twitter.com/shadcn",
     github: "https://github.com/Shatlyk1011/edil-ozi",
   },
 };
