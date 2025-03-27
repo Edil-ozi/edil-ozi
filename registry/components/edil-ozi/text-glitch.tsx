3d46a807ce2dbcbeac57af4647f4125d676ec687
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-03-27 23:57:13 +0500

    fix text glitch bug

--- registry/components/edil-ozi/text-glitch.tsx
+++ registry/components/edil-ozi/text-glitch.tsx
@@ -9,7 +9,7 @@ const TextGlitch: FC<Props> = ({ text }) => {
   text;
   return (
     <div className="group relative inline-block overflow-hidden">
-      <span className="invisible">Text Glitch Effect</span>
+      <span className="invisible">{text}</span>
       <span className="absolute left-0 top-0 text-zinc-800 transition duration-300 ease-in-out group-hover:-translate-y-full dark:text-gray-400">
         {text}
       </span>
